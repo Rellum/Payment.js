e1c51f83dc4df5159257411ec71f9b4d2f60a8be
vaughan <vaughan@luno.com>
2021-04-23 13:43:06 +0200

    Update

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+docker stop payment-js
+docker image rm payment-js:dev
 docker build -t payment-js:dev .
 docker run \
   --rm \
-  --publish 80 \
-  --publish 443 \
+  --publish 80:80 \
+  --publish 443:443 \
   --name payment-js \
   payment-js:dev
